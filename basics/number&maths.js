f68ca980c016e937f1c7b22c5b3cc3205f25dc6a
SohaniKajla <118504210+SohaniKajla@users.noreply.github.com>
2024-03-23 13:36:37 +0000

    string

---
+++ basics/number&maths.js
@@ -0,0 +1,53 @@
+const score= 300
+// console.log(score);
+
+
+
+const balance = new Number(100.4);
+console.log(balance);
+
+
+// console.log( balance.toString().length);
+// console.log( balance.toFixed(2));
+
+
+const otherNumber =  23.45644
+
+// console.log(otherNumber.toPrecision(4));
+
+
+const tens = 100000000
+// console.log(tens.toLocaleString('en-IN'));
+
+
+ //++++++++++++++++++++MATHS+++++++++++++++++++++++++++++++++
+
+
+ console.log(Math);
+ console.log(Math.abs(4));
+ console.log(Math.round(4.6));
+ console.log(Math.ceil(4.6));
+ console.log(Math.floor(4.6));
+ console.log(Math.max(4, 4,5, 88));
+ console.log(Math.random());
+ console.log(Math.random());
+ console.log((Math.random()*10)+1);
+ console.log(Math.random());
+
+
+
+ const max=10
+ const min =20
+
+
+ console.log(Math.floor(Math.random() * (max -min +1)) +min);
+
+
+
+
+
+
+ 
+
+
+

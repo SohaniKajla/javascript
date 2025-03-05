02c83e94ef83c94efd2c6bef13384dfe903233aa
SohaniKajla <118504210+SohaniKajla@users.noreply.github.com>
2025-03-05 18:40:13 +0000

    new

---
+++ Mern_stack/first_class.js
@@ -0,0 +1,18 @@
+var a=10;
+var a=12;
+//Declaration & Initialization-->Var
+let x=11;
+x=22;
+//Re-declaration is not possible but re-assign is.....-->Let
+
+const y=23;
+// const y=12;
+//Re-declaration & Re-assignment is not possible
+
+
+console.log(a);
+console.log(a);
+console.log(x);
+console.log(x);
+console.log(y);
+// console.table([a,a,x,x,y]);

df8d3cc7040214cd0dd40a8772bdcb39fc2df292
SohaniKajla <118504210+SohaniKajla@users.noreply.github.com>
2025-03-20 20:31:27 +0000

    Number & String

---
+++ Mern_stack/number.js
@@ -0,0 +1,13 @@
+let num = 12;
+console.log(num);
+
+let anotherNum =new Number(100);
+console.log(anotherNum);
+console.log(anotherNum.toString());
+console.log(anotherNum.toFixed(4));
+
+let Num= 10000000
+console.log(Num.toLocaleString("en-IN"));
+
+const numPrecision = 1234.7423
+console.log(numPrecision.toPrecision(4));
--- Mern_stack/string.js
+++ Mern_stack/string.js
@@ -6,4 +6,19 @@ let gameName=new String( "hitesh");
 console.log(gameName.toUpperCase())  ////////// to see all the properties of string go to console and declare a string and execute it  ///////////////////
 // console.log(gameName.__proto__)
 console.log(gameName.charAt(3))
-console.log(gameName.indexOf('s'))
+console.log(gameName.indexOf('s'));
+
+const newString = "Sohani Kajla";
+console.log(newString.substring(0,4));
+// console.log(newString.slice(-9,2));
+
+const anotherString= "    Sohani      ";
+console.log(anotherString);
+console.log(anotherString.trim());
+
+const anotherOneString = "Sohani Kajla"
+console.log(anotherOneString.replace(" ","-"));
+// console.log(anotherOneString.fontsize(4))
+// console.log(valueOf(anotherOneString));
+
+

b411f88396487f3e367a85b7b1f169f863a7cc32
SohaniKajla <118504210+SohaniKajla@users.noreply.github.com>
2025-03-22 11:44:57 +0000

    maths

---
+++ Mern_stack/maths.js
@@ -0,0 +1,17 @@
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.76));
+// console.log(Math.ceil(4.7));
+// console.log(Math.floor(4.2));
+// console.log(Math.pow(4,2));
+// console.log(Math.max(3,2,4,56,4));
+// console.log(Math.min(3,2,4,56,4));
+
+
+////////////////////////////////////// RANDOM METHOD  /////////////////////////////////////////////////
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log((Math.floor(Math.random()*10)+1));
+
+const max=20;
+const min=10;
+console.log((Math.floor(Math.random()*(max-min+1))+min))
